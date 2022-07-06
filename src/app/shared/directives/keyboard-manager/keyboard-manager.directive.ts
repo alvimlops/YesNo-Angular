ff465cc134efb87b03b7121e99e7ae8c51af1965
Alvaro Rodrigues <alvim.lopez@gmail.com>
2022-07-05 21:06:04 -0300

    add keyboard item

--- src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
+++ src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
@@ -1,9 +1,12 @@
-import { Directive, HostListener } from "@angular/core";
+import { KeyboardManagedItemDirective } from './keyboar-manager-item.directive';
+import { ContentChild, ContentChildren, Directive, HostListener, QueryList } from "@angular/core";
 
 @Directive({
   selector: '[appKm]'
 })
 export class KeyboarManagerDirective{
+
+  @ContentChildren(KeyboardManagedItemDirective) public items: QueryList<KeyboarManagerDirective> = null;
 
   @HostListener('keyup', ['$event'])
   public manageKeys(event: KeyboardEvent): void {
--- src/app/shared/directives/keyboard-manager/keyboard-manager.module.ts
+++ src/app/shared/directives/keyboard-manager/keyboard-manager.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core";
+import { KeyboardManagedItemDirective } from './keyboar-manager-item.directive';
 import { KeyboarManagerDirective } from './keyboard-manager.directive';
 
 @NgModule({
-  declarations: [KeyboarManagerDirective],
+  declarations: [KeyboarManagerDirective, KeyboardManagedItemDirective],
   imports: [CommonModule],
-  exports: [KeyboarManagerDirective]
+  exports: [KeyboarManagerDirective, KeyboardManagedItemDirective]
 })
 export class KeybooarManagerModule{}
